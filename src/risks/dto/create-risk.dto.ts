1b3280398d664047e74b13df580292475179e3e4
Mirna Mohsen <74990049+merna-elsherief@users.noreply.github.com>
2025-11-25 17:34:21 +0200

    Swagger Added

--- src/risks/dto/create-risk.dto.ts
+++ src/risks/dto/create-risk.dto.ts
@@ -1,19 +1,24 @@
 import { IsString, IsNotEmpty, IsInt, Min, Max } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger'; // استيراد
 
 export class CreateRiskDto {
+  @ApiProperty({ example: 'Server Overheating', description: 'The title of the risk' })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ example: 'The server room AC is failing...', description: 'Detailed description' })
   @IsString()
   @IsNotEmpty()
   description: string;
 
+  @ApiProperty({ example: 4, minimum: 1, maximum: 5 })
   @IsInt()
   @Min(1)
   @Max(5) // مستحيل يقبل رقم أكبر من 5
   likelihood: number;
 
+  @ApiProperty({ example: 5, minimum: 1, maximum: 5 })
   @IsInt()
   @Min(1)
   @Max(5)
--- src/risks/risks.controller.ts
+++ src/risks/risks.controller.ts
@@ -4,13 +4,17 @@ import { RisksService } from './risks.service';
 import { CreateRiskDto } from './dto/create-risk.dto';
 import { Patch } from '@nestjs/common'; // زودي Patch في الـ Imports
 import { UpdateRiskStatusDto } from './dto/update-risk-status.dto';
+import { ApiTags, ApiBearerAuth, ApiOperation } from '@nestjs/swagger'; // استيراد
 
+@ApiTags('Risks') // 1. بيعمل قسم اسمه Risks في الصفحة
+@ApiBearerAuth()  // 2. بيحط علامة القفل على كل الـ Endpoints دي
 @Controller('risks')
 export class RisksController {
   constructor(private readonly risksService: RisksService) {}
 
   @UseGuards(AuthGuard('jwt')) // 1. ممنوع الدخول بدون توكن
   @Post()
+  @ApiOperation({ summary: 'Create a new risk' }) // 3. وصف لما تقفي على الزرار
   create(@Body() createRiskDto: CreateRiskDto, @Request() req) {
     // 2. req.user شايل البيانات اللي رجعناها من JwtStrategy (userId, department)
     return this.risksService.create(createRiskDto, req.user);
